11b6f51f92b84289b0523e2396d37149cde2a8a9
wheval <princegbefavor@gmail.com>
2023-09-09 15:33:23 +0100

    edit script file

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-
-
 const todayDate = new Date();
 const currentDayOfWeek = weekday[todayDate.getDay()];
 const currentUTCTimeMilliseconds = Date.now();
-console.log(todayDate);
-console.log(currentDayOfWeek);
-console.log(currentUTCTimeMilliseconds);
-
 document.getElementById('today').textContent = currentDayOfWeek;
 document.getElementById('utctime').textContent = currentUTCTimeMilliseconds;
